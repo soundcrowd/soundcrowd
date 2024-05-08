13c27abb82ec80f89c6bcd18d24e157a6b4cef0c
0xf4b1 <10889432+0xf4b1@users.noreply.github.com>
2024-05-08 22:17:06 +0200

    Base stream on grid media browser fragment

--- app/src/main/java/com/tiefensuche/soundcrowd/ui/StreamMediaBrowserFragment.kt
+++ app/src/main/java/com/tiefensuche/soundcrowd/ui/StreamMediaBrowserFragment.kt
@@ -1,18 +1,15 @@
 package com.tiefensuche.soundcrowd.ui
 
 import android.os.Bundle
-import android.support.v4.media.MediaBrowserCompat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.tiefensuche.soundcrowd.R
-import kotlin.math.round
 
-internal class StreamMediaBrowserFragment : MediaBrowserFragment() {
+internal class StreamMediaBrowserFragment : GridMediaBrowserFragment() {
 
     lateinit var mSwipeRefreshLayout: SwipeRefreshLayout
 
@@ -29,16 +26,6 @@ internal class StreamMediaBrowserFragment : MediaBrowserFragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         val rootView = super.onCreateView(inflater, container, savedInstanceState)
-        mBrowserAdapter = GridItemAdapter(requests, object : MediaItemAdapter.OnItemClickListener {
-            override fun onItemClick(item: MediaBrowserCompat.MediaItem) {
-                mMediaFragmentListener.onMediaItemSelected(item)
-            }
-        }, ContextCompat.getColor(rootView.context, R.color.colorPrimary))
-        mRecyclerView.adapter = mBrowserAdapter
-        mRecyclerView.layoutManager = GridLayoutManager(
-            rootView.context,
-            round(resources.displayMetrics.widthPixels / (resources.getDimensionPixelSize(R.dimen.media_item_height)).toFloat()).toInt()
-        )
 
         mSwipeRefreshLayout = rootView.findViewById(R.id.swipeContainer)
         mSwipeRefreshLayout.isEnabled = true
