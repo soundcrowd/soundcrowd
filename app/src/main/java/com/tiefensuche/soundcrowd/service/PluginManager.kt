9ab226a254de927871d4e283c249cb0909b65769
0xf4b1 <10889432+0xf4b1@users.noreply.github.com>
2025-03-07 00:04:39 +0100

    Drop dynamic plugin loading functionality and use build-in plugins instead

--- app/src/main/java/com/tiefensuche/soundcrowd/service/PluginManager.kt
+++ app/src/main/java/com/tiefensuche/soundcrowd/service/PluginManager.kt
@@ -5,16 +5,12 @@
 package com.tiefensuche.soundcrowd.service
 
 import android.content.Context
-import android.content.pm.PackageManager
 import android.net.Uri
-import android.util.Log
 import com.tiefensuche.soundcrowd.plugins.IPlugin
-import dalvik.system.PathClassLoader
 
 internal class PluginManager(private val context: Context) {
 
     companion object {
-        private const val PLUGIN_PACKAGE_PREFIX = "com.tiefensuche.soundcrowd.plugins"
         val plugins = HashMap<String, IPlugin>()
 
         fun handleCallback(url: Uri): Boolean {
@@ -28,55 +24,18 @@ internal class PluginManager(private val context: Context) {
             }
             return false
         }
-
-        fun loadPlugin(context: Context, packageName: String): IPlugin? {
-            return loadPlugin(context, packageName, "Plugin")
-        }
-
-        private fun <T> loadPlugin(context: Context, packageName: String, className: String): T? {
-            var plugin: T? = null
-            try {
-                val appInfo = context.packageManager.getApplicationInfo(packageName, 0)
-                val pcl = PathClassLoader(appInfo.sourceDir, context.classLoader)
-                val pluginClass = pcl.loadClass("$packageName.$className")
-                val pluginContext = getPackageContext(context, packageName)
-                plugin = pluginClass.getConstructor(Context::class.java, Context::class.java).newInstance(context, pluginContext) as T
-            } catch (e: java.lang.Exception) {
-                Log.e(PluginManager::class.java.simpleName, "error loading plugin", e)
-            }
-            return plugin
-        }
-
-        private fun getPackageContext(context: Context, packageName: String): Context? {
-            return try {
-                context.applicationContext.createPackageContext(packageName,
-                    Context.CONTEXT_INCLUDE_CODE or Context.CONTEXT_IGNORE_SECURITY)
-            } catch (e: PackageManager.NameNotFoundException) {
-                null
-            }
-        }
-
-        private fun getAppsByPrefix(context: Context, prefix: String): List<String> {
-            val packages = ArrayList<String>()
-            for (applicationInfo in context.packageManager.getInstalledApplications(0)) {
-                if (applicationInfo.packageName.startsWith(prefix)) {
-                    packages.add(applicationInfo.packageName)
-                }
-            }
-            return packages
-        }
     }
 
     var initialized = false
 
     internal fun init() {
-        // Load all plugin classes from different packages that have the package prefix
-        val pluginPackages = getAppsByPrefix(context, PLUGIN_PACKAGE_PREFIX)
-        for (pluginPackage in pluginPackages) {
-            loadPlugin(context, pluginPackage)?.let { plugin ->
-                if (!plugins.containsKey(plugin.name()))
-                    plugins[plugin.name()] = plugin
-            }
+        for (plugin in listOf(
+            com.tiefensuche.soundcrowd.plugins.soundcloud.Plugin(context),
+            com.tiefensuche.soundcrowd.plugins.spotify.Plugin(context),
+            com.tiefensuche.soundcrowd.plugins.tidal.Plugin(context),
+            com.tiefensuche.soundcrowd.plugins.youtube.Plugin(context)
+        )) {
+            plugins[plugin.name()] = plugin
         }
         initialized = true
     }
